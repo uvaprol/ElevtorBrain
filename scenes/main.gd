40d63681e51fd81632cacdabff6d31a99dac5f44
uvaprol <koryakin_ad@mail.ru>
2025-11-05 00:48:34 +0300

    elevator_doors_animation

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 var floor_tasks = []
 var elevator_tasks = []
+#var floors = []
+#
+#func _ready() -> void:
+	#for child in get_children():
+		#if child.name.find('Floor') != -1:
+			#floors.append(child)
+	#print(floors)
 
 func _process(_delta: float) -> void:
 	if len(floor_tasks) != 0 and ($Elevator.move_direction == 'up'
--- scripts/elevator.gd
+++ scripts/elevator.gd
@@ -1,32 +1,42 @@
 extends Node2D
 
-#
-var floor_y = []
+var floors = []
 var task = null
 var move_direction = null
+var open_on_floor = false
+var open_status = false
 
 func _ready() -> void:
 	for child in $"..".get_children():
 		if child.name.find('Floor') != -1:
-			floor_y.append(child.position.y)
-	#floor_y.sort()
-	print(floor_y)
+			floors.append(child)
+	print(floors)
 
 func _process(_delta: float) -> void:
-	if task and $Timer.is_stopped():
-		if position.y < floor_y[task - 1]:
+	if task and not open_on_floor:
+		if position.y < floors[task - 1].position.y:
 			position.y += 1
 			move_direction = 'down'
-		elif position.y > floor_y[task - 1]:
+		elif position.y > floors[task - 1].position.y:
 			position.y -= 1
 			move_direction = 'up'
 		else:
 			move_direction = 'stop'
-			$Timer.start()
-
+			open_on_floor = true
+			$"..".floor_tasks.erase(task)
+			$"..".elevator_tasks.erase(task)
+			$AnimatedSprite2D.play('default')
+			open_status = true
+			#$AnimatedSprite2D.play_backwards('default')
 
 func _on_timer_timeout() -> void:
-	$"..".floor_tasks.erase(task)
-	$"..".elevator_tasks.erase(task)
+	$AnimatedSprite2D.play_backwards('default')
+	open_status = false
 	move_direction = null
 	task = null
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if open_status:
+		$Timer.start()
+	else:
+		open_on_floor = false
